bbdc2238e73489e6c198f079715509566f98c7bc
Nahuel Alvarez <nahuel6195@gmail.com>
2022-02-01 19:38:13 -0300

    Add product style

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import { Figure } from 'react-bootstrap'
 const Product = ({ item }) => (
-  <div>
-    <p>{item.name}</p>
+  <div className='row shadow rounded m-3 py-2'>
+    <div className='col-7'>
+      <p>{item.name}</p>
+      <Figure.Caption>
+        Nulla vitae elit libero, a pharetra augue mollis interdum.
+      </Figure.Caption>
+    </div>
+    <div className='col-5'>
+      <Figure.Image
+        className='mb-2'
+        width={120}
+        height={120}
+        src={item.image}
+      />
+      <p>$100</p>
+    </div>
   </div>
 )
 
