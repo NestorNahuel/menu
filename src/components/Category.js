c2dec6ad81200091bb52a489db005b8ea41c098a
Nahuel Alvarez <nahuel6195@gmail.com>
2022-01-31 21:28:45 -0300

    Show category image

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Button } from 'react-bootstrap'
+import { Button, Figure } from 'react-bootstrap'
 
 const Category = ({ item, onClick }) => (
-  <div className='d-grid gap-2'>
-    <Button size='lg' onClick={() => onClick(item)} variant='outline-success'>
-      {item.name}
+  <div className='d-grid gap-2 m-3'>
+    <Button
+      className='p-1'
+      size='lg'
+      onClick={() => onClick(item)}
+      variant='outline-success'
+    >
+      <div className='row'>
+        <div className='col-8'>{item.name}</div>
+        <div className='col-4'>
+          <Figure.Image
+            className='m-0'
+            width={100}
+            height={100}
+            src={item.image}
+          />
+        </div>
+      </div>
     </Button>
   </div>
 )
