cf4d8a04bfcc5b8a2ab404c810180a30191e89ab
Nahuel Alvarez <nahuel6195@gmail.com>
2022-02-10 19:37:56 -0300

    Improve code style

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types'
 import { Spinner as BootstrapSpinner } from 'react-bootstrap'
 
 const Spinner = ({ centered }) => {
-  console.log('cenenenenene', centered)
   return (
     <div
-      className={
-        'vh-100 d-flex justify-content-center' +
-        (centered ? ' align-items-center' : '')
-      }
+      className={`vh-100 d-flex justify-content-center${
+        centered ? ' align-items-center' : ''
+      }`}
     >
       <BootstrapSpinner animation='border' variant='success' />
     </div>
