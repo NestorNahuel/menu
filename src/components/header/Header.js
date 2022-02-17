6c0c480c93cb0ccf3b42d5280d2e20909941b9df
Nahuel Alvarez <nahuel6195@gmail.com>
2022-02-17 20:02:02 -0300

    Add language header button

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,24 +4,29 @@ import { Button } from 'react-bootstrap'
 import { withTranslation } from 'react-i18next'
 
 const Header = ({ label = 'Menu', onReturn, t }) => {
-  const renderBackButton = () => {
-    if (onReturn) {
-      return (
-        <div className='col-3'>
-          <Button variant='link' onClick={onReturn}>
-            <h1 className='text-white bi bi-arrow-left-circle'></h1>
-          </Button>
-        </div>
-      )
-    }
-  }
+  const renderBackButton = () => (
+    <div className='col-2 p-0'>
+      <Button hidden={!onReturn} variant='link' onClick={onReturn}>
+        <h1 className='text-white bi bi-arrow-left-circle'></h1>
+      </Button>
+    </div>
+  )
+
+  const renderLanguageButton = () => (
+    <div className='col-2 p-0'>
+      <Button variant='link' onClick={onReturn}>
+        <h1 className='text-white bi bi-globe2'></h1>
+      </Button>
+    </div>
+  )
 
   return (
     <div className='mw-100 fixed-top position-sticky row pt-3 px-2 mb-3 bg-success text-white'>
+      {renderBackButton()}
       <div className='mb-2 col mt-1'>
         <h1>{t(label)}</h1>
       </div>
-      {renderBackButton()}
+      {renderLanguageButton()}
     </div>
   )
 }
