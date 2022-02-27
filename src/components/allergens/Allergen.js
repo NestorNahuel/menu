1d290094a3f76e99e233d18da03079e20d4ee55f
Nahuel Alvarez <nahuel6195@gmail.com>
2022-02-26 21:18:50 -0300

    Add translations to allergens

--- src/components/allergens/Allergen.js
+++ src/components/allergens/Allergen.js
@@ -2,15 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Image } from 'react-bootstrap'
 import Tooltip from '../common/Tooltip'
+import { withTranslation } from 'react-i18next'
 
-const Allergen = ({ allergen }) => (
-  <Tooltip label={allergen.name}>
+const Allergen = ({ allergen, t }) => (
+  <Tooltip label={t(allergen.name)}>
     <Image src={allergen.image} width='45' height='45' fluid />
   </Tooltip>
 )
 
 Allergen.propTypes = {
   allergen: PropTypes.object,
+  t: PropTypes.func,
 }
 
-export default Allergen
+export default withTranslation('common')(Allergen)
